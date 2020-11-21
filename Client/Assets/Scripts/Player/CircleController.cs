ac7ae4c54662d5d0e00b4eb7284f2027e1a0c031
Pyrohova <pyrohovas@ukr.net>
2020-11-21 21:08:39 +0200

    moving changed

--- Client/Assets/Scripts/Player/CircleController.cs
+++ Client/Assets/Scripts/Player/CircleController.cs
@@ -19,9 +19,21 @@ public class CircleController : MonoBehaviour
 
     public void CircleFrameUpdate(float newX, float newY, float newRadius)
     {
-        transform.position = new Vector2(newX, newY);
-        //transform.position = Vector2.Lerp(transform.position, new Vector2(newX, newY), 0.2f);
-        transform.localScale = Vector2.Lerp(transform.localScale, new Vector2(newRadius, newRadius), 0.2f);
+        StartCoroutine(MoveAndChangeSize(newX, newY, newRadius, 0.02f));
+    }
+
+    private IEnumerator MoveAndChangeSize(float newX, float newY, float newRadius, float time)
+    {
+        float elapsedTime = 0;
+
+        while (elapsedTime < time)
+        {
+            transform.position = Vector2.Lerp(transform.position, new Vector2(newX, newY), (elapsedTime / time));
+            transform.localScale = Vector2.Lerp(transform.localScale, new Vector2(newRadius, newRadius), (elapsedTime / time));
+
+            elapsedTime += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
     }
 
     public void SetPlayerStartValues(int Id, float x, float y, float radius, Color color)
